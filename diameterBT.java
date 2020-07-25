7b4d8f18438acb73f58397645b1d872ffc5342db
Jilvia D'Souza <68205944+jilviadsouza@users.noreply.github.com>
2020-07-25 17:02:21 -0400

    Diameter of a Binary Tree

--- diameterBT.java
+++ diameterBT.java
@@ -1,4 +1,4 @@
-// diameterBST
+// diameterBT
 /*
 Given a binary tree, you need to compute the length of the diameter of the tree.
  The diameter of a binary tree is the length of the longest path between any two nodes in a tree. 
